521b2c03022ffabd12a4f8f68e51a4ec9fa3ad5a
Tima421 <timallm421@gmail.com>
2025-01-17 18:14:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from typing import Optional
-
 from fastapi import FastAPI
-
+import pandas as pd
+from sql import resp
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/query")
+async def root(q):
+    msg,sql_query = resp(q)
+    return {"message" : msg , "query" : sql_query}
+#input_query = input()
